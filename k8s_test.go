3f8db7a1983047a87a77895e868bfec708b2911f
Jason Costello <jason@cofide.io>
2025-11-08 23:34:33 +0000

    Adds unit test ahead of cleaning up JWK->public key method

--- k8s_test.go
+++ k8s_test.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
+	"encoding/base64"
+	"math/big"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -43,6 +47,74 @@ func Test_extractBearer(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			got := extractBearerToken(tt.header)
 			assert.Equal(t, got, tt.want)
+		})
+	}
+}
+
+func mockRSAJWK() map[string]any {
+	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return nil
+	}
+
+	nStr := base64.RawURLEncoding.EncodeToString(privateKey.PublicKey.N.Bytes())
+
+	eBytes := big.NewInt(int64(privateKey.PublicKey.E)).Bytes()
+	eStr := base64.RawURLEncoding.EncodeToString(eBytes)
+
+	return map[string]any{
+		"kty": "RSA",
+		"n":   nStr,
+		"e":   eStr,
+	}
+}
+
+func Test_jwkToPublicKey(t *testing.T) {
+	validJWK := mockRSAJWK()
+
+	tests := []struct {
+		name    string
+		jwk     map[string]any
+		wantErr bool
+	}{
+		{
+			name:    "valid JWK",
+			jwk:     validJWK,
+			wantErr: false,
+		},
+		{
+			name: "missing n",
+			jwk: map[string]any{
+				"e": validJWK["e"],
+			},
+			wantErr: true,
+		},
+		{
+			name: "missing e",
+			jwk: map[string]any{
+				"n": validJWK["n"],
+			},
+			wantErr: true,
+		},
+		{
+			name: "invalid base64",
+			jwk: map[string]any{
+				"n": "inv@lid",
+				"e": validJWK["e"],
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := jwkToPublicKey(tt.jwk)
+			if tt.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+			assert.IsType(t, &rsa.PublicKey{}, got)
 		})
 	}
 }
